800c9143d9ac101d0d10aa510e5c6f9e79268a9c
Zhan Shu <zhan.shu@rwth-aachen.de>
2024-07-24 22:45:37 +0200

    initial commit: add Quote.js

---
+++ src/components/Quote.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Quote() {
+    return (
+        <div id="quote-box">
+            <div id="text">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, corporis.</p>
+            </div>
+            <div id="author">
+                Jane
+            </div>
+            <button id='new-quote'>New Quote</button>
+            <a href="#" id='tweet-quote'>
+                <img src="" alt="" />
+            </a>
+        </div>
+    )
+}
+
+export default Quote;

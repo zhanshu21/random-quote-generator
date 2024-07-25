1c4773b01474692098e535f308b3f3192d767379
Zhan Shu <zhan.shu@rwth-aachen.de>
2024-07-25 16:38:42 +0200

    add new function: fetch  new quote and display it, when it mounts or clicking the button

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,23 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Quote() {
-    const [quote, setQuote] = useState("");
-    const [author, setAuthor] = useState("");
+    const [quote, setQuote] = useState({});
+    
+    // const [author, setAuthor] = useState("");
+    // const handleClick = () => {
+    //     setAuthor("new Author");
+    //     setQuote("new Quote.")
+    // }
 
-    const handleClick = () => {
-        setAuthor("new Author");
-        setQuote("new Quote.")
+    useEffect(() => {
+        fetchQuote();
+    }, []);
+    
+    const fetchQuote = () => {
+        let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+        fetch(url)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            let quotes = data.quotes;
+            let index = Math.floor(Math.random() * quotes.length);
+            setQuote(quotes[index]);
+            console.log(quotes[index]);
+            console.log(index);
+        });
     }
 
     return (
         <div id="quote-box">
             <div id="text">
-                <p>{quote}</p>
+                <p>{quote.quote}</p>
             </div>
             <div id="author">
-                <p>{author}</p>
+                <p>{quote.author}</p>
             </div>
-            <button id='new-quote' onClick={handleClick}>New Quote</button>
+            <button id='new-quote' onClick={fetchQuote}>New Quote</button>
             <a href="#" id='tweet-quote'>
                 <img src="" alt="" />
             </a>
